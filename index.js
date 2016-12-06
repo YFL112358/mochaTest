1da9fc595ed943877a62f178f09bcc86b6a3cd9a
shareming7 <2395277764@qq.com>
2016-12-06 19:55:05 +0800

    modified:

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-var num;
 function getPrimeByIndex(n){
-	if(n <= 4 && n > 1) {
-		num = 2 * n - 1;	
-	} else if(n > 4) {
-		num = 2 * n + 1;	
-	} else if(n = 1) {
-		return 2;
+	if(n == 1) { return "";}
+	else if(n == 2) { return 2;}
+	else if(n == 3) { return 3;}
+	else {
+		for(i=Math.floor(Math.sqrt(n));i >= 2;i--) {
+			if(n%i !== 0 && n%2 !== 0 && n % 3 !== 0) {
+				return n;
+			}
+		}
 	}
-	return num;
-	console.log(num);
 }
-
-//var test = getPrimeByIndex(5);
+var test = getPrimeByIndex(5);
 module.exports.getPrimeByIndex = getPrimeByIndex;
--- test/test.js
+++ test/test.js
@@ -2,12 +2,11 @@ const assert = require('assert');
 const getPrimeByIndex = require('./../index').getPrimeByIndex;
 suite ('getPrimeByIndex',function(){
 	test('getPrimeByIndex should return {input: output} data',function(){
-		console.log("{")
-		for(var n = 1;n < 100;n++){
-			console.log(n+':'+getPrimeByIndex(n));
-		}
-		console.log("}");
-	//	const val = getPrimeByIndex(1)
- 	//	assert.equal(2, val);
+//		console.log("{")
+//		for(var n = 1;n < 100;n++){
+//			console.log(n+':'+getPrimeByIndex(n));
+//		}
+//		console.log("}");
+ 		assert.equal(2,getPrimeByIndex(2));
 	})
 });
